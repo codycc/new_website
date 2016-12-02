f998a5eb328c780bf3145fdc3c0e0e3d30ad27f4
Cody Condon <codycondon50@gmail.com>
2016-12-02 16:13:19 -0500

    add styling

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,75 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require bootstrap-sprockets
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+$( document ).ready(function() {
+
+$('.wrapper').fadeIn(3000);
+$('.wrapper h1').fadeIn(1000);
+$('.wrapper h5').fadeIn(2000);
+
+$('.triangle2').fadeIn(50);
+$('.triangle2').fadeOut(50);
+$('.triangle2').fadeIn(100);
+$('.triangle2').fadeOut(100);
+$('.triangle2').fadeIn(150);
+$('.triangle2').fadeOut(100);
+$('.triangle2').fadeIn(1000);
+
+  $triggered_times = 0;
+
+
+
+    $(window).on('scroll', function() {
+        var y_scroll_pos = window.pageYOffset;
+        var scroll_pos_test = 1360;             // set to whatever you want it to be
+        var scroll_pos_triangle = 300;
+        if(y_scroll_pos > scroll_pos_test && $triggered_times == 0 ) {
+          $('#one').fadeIn(100);
+          $('#one').fadeOut(100);
+            $('#one').fadeIn(3000);
+          $('#two').fadeIn(500);
+          $('#two').fadeOut(100);
+          $('#two').fadeIn(500);
+          $('#two').fadeOut(200);
+          $('#two').fadeIn(1000);
+          $('#three').fadeIn(500);
+          $('#three').fadeOut(50);
+          $('#three').fadeIn(300);
+          $('#three').fadeOut(50);
+          $('#three').fadeIn(2000);
+          $('#four').fadeIn(3000);
+          $('#four').fadeOut(10);
+          $('#four').fadeIn(500);
+          $('#five').fadeIn(7000);
+          $('#six').fadeIn(3000);
+          $('#seven').fadeIn(7000);
+          $('#seven').fadeOut(100);
+          $('#seven').fadeIn(1000);
+          $('#eight').fadeIn(4000);
+          $('#nine').fadeIn(9000);
+          $('#nine').fadeOut(500);
+          $('#nine').fadeIn(1000);
+          $triggered_times = 1
+        }
+        if(y_scroll_pos > scroll_pos_triangle) {
+          $('#triangle').fadeIn(100);
+        }
+
+    });
+
+
+});
